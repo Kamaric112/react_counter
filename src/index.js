12cbb7e1e47955086ddcd8f6a62444351d3e24b0
Kamaric112 <107142487+Kamaric112@users.noreply.github.com>
2022-08-23 21:39:00 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom'
+// import { createRoot } from "react-dom/client";
 import App from "./App"
 
-createRoot(document.getElementById("root")).render(<App />)
+// createRoot(document.getElementById("root")).render(<App />)
+
+ReactDOM.render(<App />,document.getElementById("root"))
